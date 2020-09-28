45c771f3aa9330b9836fbf4a8ee160e8d592616b
Suvamrx <suvamrx@gmail.com>
2020-09-28 06:03:02 +0530

    Initial commit

---
+++ app/src/main/java/com/is_great/pro/personalassistantbot/Settings.java
@@ -0,0 +1,28 @@
+package com.is_great.pro.personalassistantbot;
+
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import android.preference.PreferenceManager;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+
+public class Settings extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        SharedPreferences preference = PreferenceManager.getDefaultSharedPreferences(this);
+        Boolean isNightMode = preference.getBoolean(getString(R.string.settings_preference_key),false);
+        if(isNightMode){
+            setTheme(R.style.PAB);
+        }else{
+            setTheme(R.style.MainTheme);
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
